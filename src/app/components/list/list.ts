25c7827c700eba07feddc4e74d48a8fd15d12355
deepshikha <deepshikha2701@gmail.com>
2026-06-23 13:23:30 +0530

    add and remove items to list

--- src/app/components/list/list.ts
+++ src/app/components/list/list.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+interface Task {
+  text: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -9,14 +14,35 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './list.html',
   styleUrls: ['./list.css']
 })
+
 export class List {
-  items = ['Example task'];
+  items: Task[] = [];
   newItem = '';
+  showInput = false;
+
+  toggleInput() {
+    this.showInput = true;
+  }
 
   addItem() {
+    console.log('Adding item:', this.newItem);
     const value = this.newItem.trim();
     if (!value) return;
-    this.items.push(value);
+    this.items.push({ text: value, done: false });
     this.newItem = '';
+    this.showInput = false;
+  }
+
+    removeItem(index: number) {
+    this.items.splice(index, 1);
+    console.log(`Removed item at index ${index}`, this.items);
+  }
+
+  onTaskChange(index: number, event: any) {
+  const isChecked = event.target.checked;
+  console.log(`Task at index ${index} changed. Done status: ${isChecked}`);
+    if (isChecked) {
+      this.removeItem(index);
+    }
   }
 }
